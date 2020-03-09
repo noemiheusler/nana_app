a75da64d06ea896fd8f70a9f47aa50b4203f7fe1
Noemi Heusler <nlheusler@gmail.com>
2020-03-09 07:09:59 +0100

    Setup controllers and views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:intro, :discover]
 
   def profile
   end
