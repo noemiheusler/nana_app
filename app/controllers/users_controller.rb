8be3b19db7ecba3e3688953433fd901812519b3d
Noemi Heusler <nlheusler@gmail.com>
2020-03-12 12:51:50 +0100

    sign up fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,50 @@
 class UsersController < ApplicationController
   def update
-      raise
+    # update user
+
+    @user = User.find(params[:id])
+    authorize @user
+    @user.motto = params[:user][:motto]
+    @user.description = params[:user][:description]
+    @user.address = params[:user][:address]
+    @user.photo = params[:user][:photo]
+
+    # create answers
+
+    if @user.save
+
+      @answer_1 = Answer.new(user_id: @user.id, question_id: 1)
+
+      if params["question-1"] == "2"
+        @answer_1.answer = false
+      else
+        @answer_1.answer = true
+      end
+
+      @answer_1.save
+
+      @answer_2 = Answer.new(user_id: @user.id, question_id: 1)
+
+      if params["question-2"] == "2"
+        @answer_2.answer = false
+      else
+        @answer_2.answer = true
+      end
+
+      @answer_2.save
+
+      @answer_3 = Answer.new(user_id: @user.id, question_id: 1)
+
+      if params["question-3"] == "2"
+        @answer_3.answer = false
+      else
+        @answer_3.answer = true
+      end
+
+      @answer_3.save
+
+    redirect_to root_path
+
+    end
   end
 end
